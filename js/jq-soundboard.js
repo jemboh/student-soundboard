22a5e4ce7e5b03837971527f79c3d8a39f9d4483
Jeremy Marer <jmarer@cisco.com>
2017-04-25 15:13:34 +0100

    first commit

---
+++ js/jq-soundboard.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  soundManager.setup({
+    url: 'swf/',
+    flashVersion: 9,
+    preferFlash: false,
+    onready: function() {
+
+    }
+  });
+
+  $(document).on('keypress', function(e) {
+
+  });
+});
